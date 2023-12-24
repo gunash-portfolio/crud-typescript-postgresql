d5a8219416f41f6c8bbcc663396b1b6843625674
Gunesh Farzaliyev <guneshfarzaliyev@Guneshs-MacBook-Pro.local>
2023-12-25 00:29:40 +0400

    express package is added with yarn and first app is working

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express, { Application,Request,Response } from "express";
+import { send } from "process";
+
+class App {
+
+    public app: Application;
+
+    constructor() {
+        this.app = express();
+        this.routes();
+    }
+
+    protected routes():void {
+        this.app.route("/").get((req:Request,res:Response) =>{
+            res.send("Welcome life");
+        }
+
+        );
+    }
+}
+
+const myApp = new App().app;
+const port:number = 3000;
+
+myApp.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
