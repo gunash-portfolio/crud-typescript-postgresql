a6d3ac8c597aef224fdf30c57167be3ef2a695ee
Gunesh Farzaliyev <guneshfarzaliyev@Guneshs-MacBook-Pro.local>
2024-01-23 20:07:37 +0400

    Some changes are applied to Note.ts

--- src/model/Note.ts
+++ src/model/Note.ts
@@ -1,11 +1,26 @@
-import { Model } from "sequelize-typescript";
+import { Column, DataType, Model, Table} from "sequelize-typescript";
+import Database from "../config/database";
+
+
+@Table({
+    tableName: Note.NOTE_TABLE_NAME
+})
 
 export class Note extends Model {
 
 
     public static NOTE_TABLE_NAME = 'note' as string;
-    public static NOTE_TABLE_ID = 'id' as string;
+    public static NOTE_ID = 'id' as string;
     public static NOTE_NAME = 'name' as string;
     public static NOTE_DESCRIPTION = 'description' as string;
+
+
+    @Column ({ 
+        type: DataType.INTEGER,
+        primaryKey:true,
+        autoIncrement:true,
+        field: Note.NOTE_ID
+    })
+    id!: number;
 
 }
