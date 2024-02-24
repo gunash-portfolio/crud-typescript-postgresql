90425ffbcf2c774917c677d3a53756d20e8a5b3e
Gunesh Farzaliyev <guneshfarzaliyev@Guneshs-MacBook-Pro.local>
2024-02-24 16:38:32 +0400

    NoteRouter.ts completely finished

--- src/router/NoteRouter.ts
+++ src/router/NoteRouter.ts
@@ -1,3 +1,19 @@
 import BaseRoutes from "./base/BaseRouter";
 import NoteController from "../controller/NoteController";
+import validate from "../helper/validate";
+import { createNoteSchema, updateNoteSchema } from "../schema/NoteSchema";
 
+class NoteRoutes extends BaseRoutes{
+   /**
+    * routes
+ :void   */
+   public routes():void {
+    this.router.post("",validate(createNoteSchema),NoteController.create)
+    this.router.patch("/:id",validate(updateNoteSchema),NoteController.update)
+    this.router.delete("/:id",NoteController.delete)
+    this.router.get("", NoteController.findAll);
+    this.router.get("/:id", NoteController.findById);
+   } 
+}
+
+export default new NoteRoutes().router;
